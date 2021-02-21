92b0b3ec4d856df92bfd7691987ee2a2313fc11b
Rares Butean <59267118+buteanrares@users.noreply.github.com>
2021-02-21 19:54:45 +0200

    UI update

--- UI_prototype.py
+++ UI_prototype.py
@@ -5,12 +5,15 @@ window = tk.Tk()
 textBox = tk.Text(height=20,width=60)
 textBox.place(x=1,y=1)
 
+messageLabel = tk.Label(text="Message:")
+messageLabel.place(x=0,y=340)
+
 messageEntry = tk.Entry(width=20)
-messageEntry.place(x=0,y=340)
+messageEntry.place(x=0,y=360)
 
 sendButton = tk.Button(text="Send")
-sendButton.place(x=160,y=340)
+sendButton.place(x=160,y=360)
 
-window.minsize(650,430)
+window.minsize(650,460)
 
 window.mainloop()
